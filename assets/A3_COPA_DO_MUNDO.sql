55a6bb6cc1a0b475467c553d416b338834d710f2
Mathzera06 <102606784+Mathzera06@users.noreply.github.com>
2022-12-01 20:57:37 -0300

    Update A3_COPA_DO_MUNDO.sql

--- assets/A3_COPA_DO_MUNDO.sql
+++ assets/A3_COPA_DO_MUNDO.sql
@@ -87,5 +87,38 @@ BEGIN
 END //
 DELIMITER ;
 
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(1,2,0,2,DATE("2022-11-29"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(3,4,1,3,DATE("2022-11-29"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(5,6,1,0,DATE("2022-11-28"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(7,8,2,1,DATE("2022-11-28"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(9,10,1,0,DATE("2022-11-27"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(11,12,3,0,DATE("2022-11-27"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(13,14,1,3,DATE("2022-11-26"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(15,16,3,0,DATE("2022-11-26"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(17,18,2,1,DATE("2022-11-25"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(19,20,1,2,DATE("2022-11-25"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(21,22,1,1,DATE("2022-11-24"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(23,24,2,0,DATE("2022-11-24"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(25,26,1,2,DATE("2022-11-23"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(27,28,1,1,DATE("2022-11-22"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(29,30,1,3,DATE("2022-11-21"));
+INSERT INTO TB_PARTIDA (id_time1, id_time2, qtd_gols_time1, qtd_gols_time2, dt_partida) VALUES
+(31,32,1,2,DATE("2022-11-20"));
+
 /* Fim Criação das Procs */
 
